79d7e4198189c5912752a2a37886f36dc0cd42b9
Erika Szabo <erika.szabo.geo@gmail.com>
2020-04-07 16:45:47 -0400

    added map component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from 'react'
+import mapboxgl from 'mapbox-gl'
+import useSWR from 'swr' // React hook to fetch the data
+import lookup from 'country-code-lookup' // npm module to get ISO Code for countries
+import './App.scss'
 
+// Mapbox css - neede to make tooltips work later in the article
+
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API_TOKEN
 function App() {
+  // DOM element to render map
+  const mapboxElRef = useRef(null) 
+
+  // Initialize our map
+  useEffect(() => {
+    // You can store the map indtance with useRef too
+    const map = new mapboxgl.Map({
+      container: mapboxElRef.current,
+      style: 'mapbox://styles/notalemesa/ck8dqwdum09ju1ioj65e3ql3k',
+      center: [16, 27], //initial geolocation
+      zoom: 2 //initial zoom
+    })
+
+    // Add navigation controls to the top right of the canvas
+    map.addControl(new mapboxgl.NavigationControl())
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='mapContainer'>
+        {/* Assigned mapbox container */}
+        <div className='mapBox' ref={ mapboxElRef } />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
