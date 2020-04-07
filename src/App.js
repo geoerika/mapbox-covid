f98bc9aaf731a500468cbc179b66728d4f12f5c7
Erika Szabo <erika.szabo.geo@gmail.com>
2020-04-07 17:12:59 -0400

    added circle feature layer on the map

--- src/App.js
+++ src/App.js
@@ -8,22 +8,73 @@ import './App.scss'
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API_TOKEN
 function App() {
-  // DOM element to render map
-  const mapboxElRef = useRef(null) 
+  const mapboxElRef = useRef(null); // DOM element to render map
+  const fetcher = url =>
+    fetch(url)
+      .then(r => r.json())
+      .then(data =>
+        data.map((point, index) => ({
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: [
+              point.coordinates.longitude,
+              point.coordinates.latitude
+            ]
+          },
+          properties: {
+            id: index, // unique identifier in this case the index
+            country: point.country,
+            province: point.province,
+            cases: point.stats.confirmed,
+            deaths: point.stats.deaths
+          }
+        }))
+      )
 
-  // Initialize our map
+  // Fetching our data with swr package
+  const { data } = useSWR('https://corona.lmao.ninja/v2/jhucsse', fetcher) 
+
   useEffect(() => {
-    // You can store the map indtance with useRef too
-    const map = new mapboxgl.Map({
-      container: mapboxElRef.current,
-      style: 'mapbox://styles/notalemesa/ck8dqwdum09ju1ioj65e3ql3k',
-      center: [16, 27], //initial geolocation
-      zoom: 2 //initial zoom
-    })
+    if (data) {
+      // You can store the map instance with useRef too
+      const map = new mapboxgl.Map({
+        container: mapboxElRef.current,
+        style: 'mapbox://styles/notalemesa/ck8dqwdum09ju1ioj65e3ql3k',
+        center: [16, 27], //initial geolocation
+        zoom: 2 //initial zoom
+      })
+      // Add navigation controls to the top right of the canvas
+      map.addControl(new mapboxgl.NavigationControl())
 
-    // Add navigation controls to the top right of the canvas
-    map.addControl(new mapboxgl.NavigationControl())
-  }, [])
+      // Call this method when the map is loaded
+      map.once('load', () => {
+        // Add our source
+        // with id 'points'
+        map.addSource('points', {
+          type: 'geojson',
+          data: {
+            type: 'FeatureCollection',
+            features: data
+          }  
+        })
+
+        // Add our layer
+        map.addLayer({
+          id: "circles",
+          source: "points", // this should be the id of the source
+          type: "circle",
+          // paint properties
+          paint: {
+            "circle-opacity": 0.75,
+            "circle-stroke-width": 1,
+            "circle-radius": 4,
+            "circle-color": "#FFEB3B"
+          }
+        })
+      })
+    }
+  }, [data])
   return (
     <div className='App'>
       <div className='mapContainer'>
